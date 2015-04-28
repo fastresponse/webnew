4638fb53b8516d48c6db1a342f9fe55bd05ccd5d
Brian Bridge <bbridge@fastresponse.org>
2015-04-28 15:07:38 -0700

    initial commit

---
+++ header.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title><?= $title || '' ?></title>
+    <meta name="description" content="<?= $description || '' ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+  </head>
+  <body>
+    <div class="header-container">
+      <header class="wrapper clearfix">
+      <h1 class="title"><?= $page_header || '' ?></h1>
+        <nav>
+          <ul>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Career Courses</a></li>
+            <li><a href="#">Continuing Education</a></li>
+            <li><a href="#">Student Resources</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+
+    <div class="main-container">
+      <div class="main wrapper clearfix">
+
